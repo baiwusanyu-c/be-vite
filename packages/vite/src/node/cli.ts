fbaee0efe721cc0b301f2e0dc8621d06ae1e8490
baiwusanyu <740132583@qq.com>
2022-09-24 22:32:12 +0800

    update：添加了一些 cac 的使用注释

--- packages/vite/src/node/cli.ts
+++ packages/vite/src/node/cli.ts
@@ -7,15 +7,15 @@ import { startDevServer } from "./server";
 const cli = cac()
 
 // 指令
-cli
-    .command("[root]", "Run the development server")
-    .alias("serve")
-    .alias("dev")
-    .action(async () => {
+cli.command("[root]", "Run the development server")
+    // 创建一个指令实例，设置为 [root],你的bin 是 x ，则使用x可运行，否则要用 x serve 、x rawName、x dev
+    .alias("serve") // 指令别名
+    .alias("dev") // 指令别名
+    .action(async () => { // 指令动作
         // 启动一个 dev 服务
         await startDevServer();
     });
 
 cli.help();
 
-cli.parse();
+cli.parse(); // 解析上面步骤配置，开始执行
